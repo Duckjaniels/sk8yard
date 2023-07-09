3ba27caa814d9fa0315e07e1f8a93324bd0544f8
Duckjaniels <maksym73@gmail.com>
2023-07-10 00:56:57 +0200

    Contact page styling finished

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,12 +2,14 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import heroImage from "../assets/hero_contact.png";
 import Footer from "../components/Footer";
+import ContactForm from "../components/ContactForm";
 
 function Contact() {
   return (
     <>
       <Navbar />
       <Hero cName="hero-mid" heroImg={heroImage} title="Kontakt" />
+      <ContactForm />
       <Footer />
     </>
   );
