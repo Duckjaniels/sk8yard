cf3b2f2a960ec33ecc12b715442b680cfe560c87
Duckjaniels <maksym73@gmail.com>
2023-08-30 15:40:03 +0200

    Reorginizing and styling OurOffer component

--- src/components/OurOffer.js
+++ src/components/OurOffer.js
@@ -1,39 +1,88 @@
 import "./OurOfferStyles.css";
 import OurOfferData from "./OurOfferData";
-import Offert1 from "../assets/offer1.png";
-import Offert2 from "../assets/offer2.png";
-import Offert3 from "../assets/offer3.png";
+import Offert1 from "../assets/offer_1.webp";
+import Offert2 from "../assets/offer_2.webp";
+import Offert3 from "../assets/offer_3.webp";
+import Offert4 from "../assets/offer_4.webp";
+import Offert5 from "../assets/offer_5.webp";
+import Offert6 from "../assets/offer_6.webp";
 
 function OurOffer() {
   return (
     <div className="offer">
-      <h1>oferta</h1>
-
-      <p>
-        Od planowania i projektowania, po produkcję i budowę, aż po organizację
-        eventów SK8YARD to punkt kompleksowej obsługi wszystkich skateparków, w
-        tym napraw i konserwacji.
-      </p>
+      <h1 className="full-width">oferta</h1>
+      <p className="offer-subtitle">Kompleksowa obsługa na każdym etapie</p>
       <div className="offercard">
-        <OurOfferData
-          image={Offert1}
-          heading="Konsultacje"
-          text="Zapewniamy specjalistyczną wiedzę w zakresie planowania budowy skateparku. Niezależnie od tego, czy chodzi o dobór urządzeń, wybór odpowiedniej technologii czy lokalizacji, staramy się zaoferować najlepszą pomoc."
-        />
-        <OurOfferData
-          image={Offert2}
-          heading="Projektowanie"
-          text="Niewątpliwym sukcesem każdego skateparku jest jego unikalny projekt dopasowany do miejsca i potrzeb lokalnych użytkowników. Cieszymy się, że możemy dostarczyć kompleksową usługę od wizualizacji 3D, rysunków technicznych po pełną dokumentację projektową."
-        />
-        <OurOfferData
-          image={Offert3}
-          heading="Budowa skateparków"
-          text="SK8YARD to jeden z najbardziej doświadczonych na rynku dostawców urządzeń skateparku. Posiadamy swój unikalny zestaw umiejętności który pozwala nam na budowę skateparku w niemal każdej dostępnej na rynku technologii."
-        />
+        <div className="o-card o-card-image">
+          <h4 className="o-card-title">Planowanie</h4>
+          <img src={Offert1} alt="Konsultacje" />
+          <p className="o-card-text">
+            Firma sk8yard zapewnia specjalistyczną wiedzę w zakresie planowania
+            budowy skateparku.
+          </p>
+          <a href="/Offer" className="offer-button">
+            Czytaj więcej
+          </a>
+        </div>
+
+        <div className="o-card o-card-image">
+          <h4 className="o-card-title">Projektowanie</h4>
+          <img src={Offert2} alt="Projektowanie" />
+          <p className="o-card-text">
+            Niewątpliwym sukcesem każdego skateparku jest jego unikalny projekt
+            dopasowany do miejsca oraz potrzeb lokalnych mieszkańców i
+            użytkowników.
+          </p>
+          <a href="/Offer" className="offer-button">
+            Czytaj więcej
+          </a>
+        </div>
+
+        <div className="o-card o-card-image">
+          <h4 className="o-card-title">Budowa </h4>
+          <img src={Offert3} alt="Budowa skateparków" />
+          <p className="o-card-text">
+            Firma SK8YARD to jeden z najbardziej doświadczonych na rynku
+            producentów urządzeń skateparku.
+          </p>
+          <a href="/Offer" className="offer-button">
+            Czytaj więcej
+          </a>
+        </div>
+        <div className="o-card o-card-image">
+          <h4 className="o-card-title">Pumptracki modułowe</h4>
+          <img src={Offert4} alt="Pumptracki modułowe" />
+          <p className="o-card-text">
+            Doskonale zaprojektowane i łatwe w montażu Modułowe Tory Pumptracki.
+          </p>
+          <a href="/Offer" className="offer-button">
+            Czytaj więcej
+          </a>
+        </div>
+        <div className="o-card o-card-image">
+          <h4 className="o-card-title">Urządenia mobilne</h4>
+          <img src={Offert5} alt="Urządenia mobilne" />
+          <p className="o-card-text">
+            SK8YARD oferuje kompleksową usługę od projektu koncepcyjnego po
+            realizację urządzeń na potrzeby własne i komercyjne
+          </p>
+          <a href="/Offer" className="offer-button">
+            Czytaj więcej
+          </a>
+        </div>
+        <div className="o-card o-card-image">
+          <h4 className="o-card-title">Serwis i modernizacje</h4>
+          <img src={Offert6} alt="Serwis i modernizacje" />
+          <p className="o-card-text">
+            Oferujemy pełny pakiet serwisowy od przeglądów gwarancyjnych przez
+            naprawy i konserwację aż po Kompleksową naprawę i modernicazję
+            skateparków.
+          </p>
+          <a href="/Offer" className="offer-button">
+            Czytaj więcej
+          </a>
+        </div>
       </div>
-      <a href="/Offer" className="offer-button">
-        Zobacz więcej
-      </a>
     </div>
   );
 }
