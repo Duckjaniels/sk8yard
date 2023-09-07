28ebf0175e6ed2ad17ee8489b8e01c9e676b9fee
Duckjaniels <maksym73@gmail.com>
2023-09-07 17:52:34 +0200

    contactForm new layout

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,15 +3,42 @@ import "./ContactFormStyles.css";
 function ContactForm() {
   return (
     <div className="form-container">
-      <h1>Skontaktuj się z nami !</h1>
-      <form>
-        <input placeholder="Imię" />
-        <input placeholder="E-mail" />
-        <input placeholder="Temat" />
-        <input placeholder="Telefon" />
-        <textarea placeholder="Wiadomość" rows="4" />
-        <button>Wyślij</button>
-      </form>
+      <h1 className="full-width ">Kontakt</h1>
+      <div className="contact">
+        <form>
+          <h4 className="contact-title">Formularz kontaktowy</h4>
+          <input placeholder="Imię" />
+          <input placeholder="E-mail" />
+          <input placeholder="Temat" />
+          <input placeholder="Telefon" />
+          <textarea placeholder="Wiadomość" rows="4" />
+          <button>Wyślij</button>
+        </form>
+        <div className="contact_info">
+          <div className="contact_info-up">
+            <p className="contact_info-text">
+              Potrzebujesz pomocy lub chcesz nawiązać z nami współprace ?
+            </p>
+            <p className="contact_info-text">
+              ZADZWOŃ, NAPISZ lub skorzystaj z FORMULARZA KONTAKTOWEGO
+            </p>
+          </div>
+          <div className="contact_info-down">
+            <div className="contact_info-down-left">
+              <h4 className="contact-title down">Informacje ogólne</h4>
+              <p className="contact_info-text">
+                info@sk8yard.com (+48) 785-066-111
+              </p>
+            </div>
+            <div className="contact_info-down-right">
+              <h4 className="contact-title down">Projekty i konsultacje</h4>
+              <p className="contact_info-text">
+                projekty@sk8yard.com (+48) 883-044-014
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
