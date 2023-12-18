dccb96705b88423bf166908af39afd9b5be82cad
Duckjaniels <maksym73@gmail.com>
2023-12-18 17:03:57 +0100

    Styling improvements for tablets

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './routes/Home.js';
-import About from './routes/About.js';
-import Offer from './routes/Offer.js';
-import Gallery from './routes/Gallery.js';
-import Contact from './routes/Contact.js';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./routes/Home.js";
+import About from "./routes/About.js";
+import Offer from "./routes/Offer.js";
+import Gallery from "./routes/Gallery.js";
+import Contact from "./routes/Contact.js";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/offer" element={<Offer/>}/>
-        <Route path="/gallery" element={<Gallery/>}/>
-        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/offer" element={<Offer />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,27 +1,28 @@
 export const MenuItems = [
   {
-    title : "Start",
-    url : "/",
-    cName : "nav-links"
+    title: "Start",
+    url: "/",
+    cName: "nav-links",
+  },
+
+  {
+    title: "Oferta",
+    url: "/offer",
+    cName: "nav-links",
   },
   {
-    title : "O nas",
-    url : "/about",
-    cName : "nav-links"
+    title: "Galeria",
+    url: "/gallery",
+    cName: "nav-links",
   },
   {
-    title : "Oferta",
-    url : "/offer",
-    cName : "nav-links"
+    title: "O nas",
+    url: "/about",
+    cName: "nav-links",
   },
   {
-    title : "Galeria",
-    url : "/gallery",
-    cName : "nav-links"
-  },
-  {
-    title : "Kontakt",
-    url : "/contact",
-    cName : "nav-links"
+    title: "Kontakt",
+    url: "/contact",
+    cName: "nav-links",
   },
 ];
